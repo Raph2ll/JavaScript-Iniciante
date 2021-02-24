b43dff22d433a21ac14c6dddc54854b9804442e8
Raph2ll <66336767+Raph2ll@users.noreply.github.com>
2021-02-24 20:43:19 -0300

    Update script.js

--- Aula12ex/ex15/script.js
+++ Aula12ex/ex15/script.js
@@ -1,11 +1,9 @@
+var img = document.querySelector('div#imagen')
 function carregar (){
-    var pao = 123
-    var ojo = document.querySelector('div#msg')
-    var img = document.querySelector('div#imagen')
+
     var msg = document.querySelector('div#msg')
     var data = new Date()
     var hora = data.getHours()
-    var minuto = data.getMinutes()
-    msg.innerHTML = `Agora são ${hora}:${minuto} horas.`
+    msg.innerHTML = `Agora são ${hora}horas.`
     
 }
