ea33591040e1e3344599218a29e3e92821ebf626
Raph2ll <66336767+Raph2ll@users.noreply.github.com>
2021-02-26 19:18:31 -0300

    Aula 13

---
+++ Aula13/ambiente.js
@@ -0,0 +1,12 @@
+var pizza = 0
+    while (pizza < 20) {
+        console.log(`Eu comi ${pizza}fatias`)
+        pizza++// pizza recebe pizza +1
+/*
+"Usando o Do"
+do {
+        console.log(`Eu comi ${pizza}fatias`)
+        pizza++
+    } while (pizza < 20) 
+*/
+
