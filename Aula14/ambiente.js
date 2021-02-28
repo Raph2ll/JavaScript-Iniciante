6ae5db571508cd31ae9e3d4c9018864bc228197c
Raph2ll <66336767+Raph2ll@users.noreply.github.com>
2021-02-28 10:52:19 -0300

    Aula 14

---
+++ Aula14/ambiente.js
@@ -0,0 +1,16 @@
+/*
+var c = 1 
+var d = 2
+while (c<=10){
+    console.log(c)
+    c += d
+}
+Agora usando o For
+*/
+ console.log('Números Impares de 1 a 10')
+for (c = 1; c<=10; c+= 2){
+    /* lembrando que "c" é uma variável 
+    que foi criada no bloco for */
+    console.log(c)
+ }
+ console.log('FIM!')
